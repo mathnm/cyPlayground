15b0d0d1514b58d4167b1e2f32474923dfe04b84
Matheus <matheusnuma@gmail.com>
2024-10-18 19:43:35 -0300

    inicio do projeto

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,13 @@
+describe('Login', () => {
+  it('Deve logar com sucesso', () => {
+    cy.viewport(1920, 1080)
+    cy.visit('https://playground.cyskills.com.br')
+
+    cy.get('[data-cy="email"]').type('papito@cyskills.com.br')
+    cy.get('[data-cy="password"]').type('showtime')
+
+    cy.get('[data-cy="login-button"]').click()
+
+    cy.get('[data-cy="welcome-title"]').should('be.visible').and('have.text', 'Boas vindas ao Cypress Playground')
+  })
+})
