03051b69283de47ee0c089a0563fb71e35097752
Matheus <matheusnuma@gmail.com>
2024-10-31 09:02:58 -0300

    Select

---
+++ cypress/e2e/select.cy.js
@@ -0,0 +1,31 @@
+describe('Select', () => {
+
+    beforeEach(() => {
+        cy.goHome()
+        cy.login('papito@cyskills.com.br', 'showtime')
+        cy.userLoggedIn()
+        cy.goTo('/select', 'Select')
+    })
+
+    it('Deve selecionar o Cypress', () => {
+        cy.contains('label', 'Selecione um Framework de Testes')
+            .parent()
+            .find('select')
+            .select('Cypress')
+    })
+
+    it.only('Deve escolher as linguagens que usam node.js', ()=>{
+
+        const langs = ['JavaScript', 'TypeScript']
+
+        cy.get('input[placeholder^="Linguagens de programação"]')
+            .click()
+
+        langs.forEach(lang =>{
+            cy.contains('.option-item', lang)
+                .click() 
+        })
+    })
+
+
+})
